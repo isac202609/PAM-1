d527c5ae306a0743ee0b3f41b934410207bfe4ed
isac202609 <isac202609@gmail.com>
2026-09-03 19:00:48 +0000

    final Exemplo2

---
+++ components/Titulo/estilo.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+
+const estilo = StyleSheet.create({
+  box: {
+    backgroundColor: '#000',
+    alignItems: 'center',
+    padding: 10,
+  },
+  texto:{
+    fontSize: 28,
+    color: '#61dafb'
+  }
+});
